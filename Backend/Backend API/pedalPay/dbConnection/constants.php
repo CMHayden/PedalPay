5650d1190fcf5f860fe2813d3d69bb82a0795d60
cmh1 <haydencallum4@gmail.com>
2019-03-30 14:53:01 +0000

    Submitted version
    This is the code that was submitted for our deliverable. Any changes from this point out is not graded, and is only done for self improvement and for the learning experience.

---
+++ Backend/Backend API/pedalPay/dbConnection/constants.php
@@ -0,0 +1,9 @@
+
+<?php
+
+define('DB_HOST', 'db759395121.hosting-data.io');
+define('DB_USERNAME', 'dbo759395121');
+define('DB_PASSWORD', 'vEhkah-ryfdoz-gyvme2');
+define('DB_NAME', 'db759395121');
+
+?>
